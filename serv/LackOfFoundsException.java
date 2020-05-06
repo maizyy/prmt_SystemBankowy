e4249b2c51a1b08fbd80bfccdb7d3badedd22ff6
JakubNajman <62356821+JakubNajman@users.noreply.github.com>
2020-05-06 22:33:21 +0200

    Update LackOfFoundsException.java

--- serv/LackOfFoundsException.java
+++ serv/LackOfFoundsException.java
@@ -1,7 +1,10 @@
 package prmt_SystemBankowy;
 
 public class LackOfFoundsException extends Exception{
-    public LackOfFoundsException(){
+
+    private static final long serialVersionUID = 1L;
+
+    public LackOfFoundsException() {
         System.out.println("Brak wystarczających środków na koncie na przeprowadzenie takie operacji.");
     }
     public LackOfFoundsException(String errorMessege, Throwable err){
