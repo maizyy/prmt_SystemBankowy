134ce7891e21f2dc027b10ab149f94c7360cc76f
Mateusz Izbicki <m.izbicki@stud.elka.pw.edu.pl>
2020-05-06 21:18:01 +0200

    Zmiany w Auth

---
+++ src/Authorization.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Authorization {
+    private byte counter= 0;
+    private boolean getid() throws InterruptedException {
+
+        Scanner console = new Scanner(System.in);
+        Random r = new Random();
+        int pass = (1000+r.nextInt(9999))%10000;
+        System.out.println("Twoje haslo bezpieczenstwa: "+pass);
+        System.out.print("Wpisz haslo by potwierdzic: ");
+        int checkPass = console.nextInt();
+        byte counterLimit = 2;
+        if ((checkPass == pass) && (counter < counterLimit)){
+            System.out.println("Pomyslna autoryzacja !!!!");
+            return true;
+        }
+        else if(counter >= counterLimit){
+            System.out.println("Zawieszony dostep poczekaj 5s");
+            return false;
+        }
+        else {
+            System.out.println("Sprobuj ponownie");
+            counter++;
+            TimeUnit.SECONDS.sleep(2);
+            getid();
+        }
+        return false;
+    }
+    public static void main(String[] args) throws InterruptedException {
+	    Authorization a = new Authorization();
+        System.out.println(a.getid());
+    }
+}
